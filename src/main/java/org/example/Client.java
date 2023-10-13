fde2f8ae44535bedc1ae674cf87039b540eb9678
Mark Bilous <belousme601@gmail.com>
2023-10-13 09:52:10 +0300

    add string address, number for client

--- src/main/java/org/example/Client.java
+++ src/main/java/org/example/Client.java
@@ -3,10 +3,14 @@ package org.example;
 public class Client {
     private int id;
     private String name;
+    private String address;
+    private String number;
 
-    public Client(int id, String name) {
+    public Client(int id, String name, String address, String number) {
         this.id = id;
         this.name = name;
+        this.address = address;
+        this.number = number;
     }
 
     public int getId(){
@@ -15,5 +19,13 @@ public class Client {
 
     public String getName() {
         return name;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+
+    public String getNumber(){
+        return number;
     }
 }
